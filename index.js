c368965f1ea24c96b0ebbd140b79eba93c0e64e6
nguyen nam <16520775@gm.uit.edu.vn>
2020-09-23 16:57:02 +0700

    23-0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express=require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const userRoute=require('./routes/user.route');
 const app=express();
 const port=3000;
 app.use(bodyParser.json()) // for parsing application/json
@@ -8,36 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-var users=[
-    { id:1,name:'name'},
-    { id:2, name:'ngoc'}];
-app.get('/users',function(req, res){
-    res.render('users/index',{
-        users: users
-    });
-    
-});
-
-app.get('/users/search',function(req,res){
-    var q=req.query.q;
-    var matchUsers=users.filter(function(user){
-        return user.name.indexOf(q)!==-1;
-    });
-    res.render('users/index',{
-        users: matchUsers,
-        
-    });
-
-});
-
-app.get('/users/create',function(req,res){
-    res.render('users/create');
-
-});
-app.post('/users/create',function(req,res){
-    users.push(req.body)
-    res.redirect('/users')
-});
+app.use('/users',userRoute);
 
 app.listen(port,function(){
     console.log("listen at http://localhost:"+port);
---
+++ routes/user.route.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const shortid = require('shortid');
+var db=require('../views/db');
+
+router.get('/',function(req, res){
+    res.render('users/index',{
+        users: db.get('users').value()
+    });
+    
+});
+
+router.get('/search',function(req,res){
+    var q=req.query.q;
+    var matchUsers=db.get('users').value().filter(function(user){
+        return user.name.indexOf(q)!==-1;
+    });
+    res.render('users/index',{
+        users: matchUsers
+        
+    });
+
+});
+
+router.get('/create',function(req,res){
+    res.render('users/create');
+
+});
+
+router.get('/:userId',function(req,res){
+    var id=req.params.userId;
+    var user= db.get("users").find({id:id}).value();
+    res.render('users/view',{
+        user:user
+    });
+});
+
+router.post('/create',function(req,res){
+    req.body.id=shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users')
+});
+
+module.exports=router;
